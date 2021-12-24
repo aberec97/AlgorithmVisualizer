e254dfb651ec76ff1320a3bfc72e502055bf589f
AB092394 <AB092394@cerner.net>
2021-12-24 08:53:52 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
+import Paging from './problem_pages/Paging'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main-container">
+        <h1>AlgorithmVisualizer</h1>
+        <p>Welcome! Start by selecting a problem, then choose an alogrithm to solve it!</p>
+        <div className="problem-select">
+          <h5>Online Algorithm Problems</h5>
+          <Button>Paging</Button>
+          <Button disabled>Scheduling</Button>
+          <Button disabled>Bin Packing</Button>
+
+          <h5>Sorting</h5>
+          <Button disabled>Bubble sort</Button>
+          <Button disabled>Insertion sort</Button>
+        </div>
+        
+        <div className="problems">
+          <Paging />
+        </div>
+        
+      </div>
   );
 }
 
