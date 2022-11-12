4241799de4a471ea076918544ab252d07297b33c
AB092394 <Adam_Berec@epam.onmicrosoft.com>
2022-11-12 11:20:39 +0100

    Add first step and last step buttons

--- src/common/arrows.jsx
+++ src/common/arrows.jsx
@@ -23,11 +23,24 @@ class Arrows extends Component {
         }
     };
 
+    firstStep = () => {
+        if (!this.props.history || this.props.cost === 0) return;
+        this.props.setCurrentStep(0);
+    }
+
+    lastStep = () => {
+        const input = this.props.input;
+        if (!input || input.length === 0 || !this.props.history || this.props.cost === 0) return;
+        this.props.setCurrentStep(this.props.input.length);
+    }
+
     render() {
         return (
             <div>
+                <Button variant="light" onClick={this.firstStep}>|&lt;</Button>
                 <Button variant="light" onClick={this.previousStep}>&lt;</Button>
                 <Button variant="light" onClick={this.nextStep}>&gt;</Button>
+                <Button variant="light" onClick={this.lastStep}>&gt;|</Button>
             </div>
         );
     }
