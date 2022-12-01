689dac42b1467ba901509f7d66e65e83a1e11032
AB092394 <Adam_Berec@epam.onmicrosoft.com>
2022-12-01 16:28:22 +0100

    adjust bin packing visualization

--- src/problems/bin-packing/binPackingVisualization.jsx
+++ src/problems/bin-packing/binPackingVisualization.jsx
@@ -21,13 +21,16 @@ class BinPackingVisualization extends Component {
                 bins.set(currentBin, []);
                 binItems.push([]);
             }
-            let currentItem = currentItemAndBin['item'];
+            let currentItem = Number(currentItemAndBin['item']);
+            let rounded = currentItem.toFixed(1);
+            let dotsRemoved = rounded.replace(/\./g, '');
             bins.get(currentBin).push(currentItem);
+            console.log(dotsRemoved);
             fullnessIndicator = currentItem + "em";
             binItems[currentBin].push(
                 Number(currentItem) === 10 ?
-                    <div key={i} style={{ height: fullnessIndicator }} className='bin-item'>{currentItem / 10}</div> :
-                    <div key={i} style={{ height: fullnessIndicator }} className='bin-item'>0.{currentItem}</div>);
+                    <div key={i} style={{ height: fullnessIndicator }} className='bin-item'>{dotsRemoved / 1000}</div> :
+                    <div key={i} style={{ height: fullnessIndicator }} className='bin-item'>0.{dotsRemoved}</div>);
         }
         let elements = visualizeInput(this.props.input, this.props.currentStep);
         let binsForVisualization = [];
